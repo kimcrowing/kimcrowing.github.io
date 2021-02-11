222b7d8fc6b29ff1874b4bb4a120f401f783a928
kimcrowing <kimcrowing@hotmail.com>
2021-02-12 00:36:22 +0800

    20210212

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,5 +30,20 @@
 			speed: 1000,
 			offset: function() { return $nav.height(); }
 		});
+	// GoTop
+		$window.scroll(function(){
+			var sc=$(window).scrollTop();
+			var rwidth=$(window).width()
+			if(sc>0){
+				$("#goTopBtn").css("display","block");
+				$("#goTopBtn").css("left",(rwidth-36)+"px")
+			}else{
+				$("#goTopBtn").css("display","none");
+		}
+		});
+		$("#goTopBtn").click(function(){
+			var sc=$(window).scrollTop();
+			$('body,html').animate({scrollTop:0},500);
+		})
 
 })(jQuery);
